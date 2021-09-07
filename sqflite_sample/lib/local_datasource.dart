77fe969e7b45d2f8573c5f960d0b0526d28ca925
SamTobias <13008789+SamTobias@users.noreply.github.com>
2021-09-07 15:06:12 -0300

    Refactoring database creation query

--- sqflite_sample/lib/local_datasource.dart
+++ sqflite_sample/lib/local_datasource.dart
@@ -9,15 +9,23 @@ class LocalDatasource {
       _database = await openDatabase(
         join(await getDatabasesPath(), 'note_database.db'),
         onCreate: (db, version) {
-          return db.execute(
-            "CREATE TABLE notes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL, date TEXT NOT NULL, completed INTEGER NOT NULL DEFAULT FALSE)",
-          );
+          final batch = db.batch();
+
+          _createNotesTableV1(batch);
+
+          return batch.commit();
         },
         version: 1,
       );
     }
 
     return _database!;
+  }
+
+  void _createNotesTableV1(Batch batch) {
+    batch.execute(
+      "CREATE TABLE notes(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL, date TEXT NOT NULL, completed INTEGER NOT NULL DEFAULT FALSE)",
+    );
   }
 
   Future close() async => _database?.close();
